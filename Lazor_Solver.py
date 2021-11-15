7e3678491a54e5af7bfa3519e7da9cb7bd1cfe9c
josedf2501 <92876594+josedf2501@users.noreply.github.com>
2021-11-14 20:53:33 -0500

    Update Lazor_Solver.py

--- Lazor_Solver.py
+++ Lazor_Solver.py
@@ -1,130 +1,119 @@
 
 
+import sys
 import re
 from PIL import Image, ImageDraw
-
-def read_file(filename):
-    '''This reading function will return a dictionary that will contain 
+class File:
+    '''This class File will return a dictionary that will contain 
      information about the gameboard, position of lazers and the directions.
      We will use the re module to search strings in a way that it will 
      facilitate our task of creating a dictionary with all our specifications.'''
 
-    # Check if the user entered a .bff file
-    if '.bff' not in filename:
-        print('The file you entered is not in .bff format')
-        return None
-    
+    def __init__(self, filename):
+
+        if '.bff' not in filename:
+            print('The file you entered is not in .bff format')
+            sys.exit()
+        self.ftpr = open(filename)
+
+    def read_file(self):
+        read_grid = self.ftpr.read()
+        grid = re.search('GRID START.*GRID STOP', read_grid, re.DOTALL)
         
-    bff = open(filename)
+        # read file and find the grid part
+        # Use re.DOTALL to get the part of the file from 'GRID START from *GRID STOP'
+        
+        grid_text = read_grid[grid.start():grid.end()]
+        self.ftpr.close()
+       
+        # calculate the size of our grid
+        
+        rows = 0
+        col = 0
+       
+        # We will use re.search to find the first row of the grid.
+        # The number of columns will be calculated with the number of elements
+        # in the first row.
+        
+        row = re.search('([oxABC] *)+[oxABC]', read_grid)
+        row = read_grid[row.start():row.end()]
+        row = row.replace(' ', '')
+        col = len(row)
+        
+        # creat a list and make each line of grid a element
+        # to calculate the number of rows
+       
+        board = grid_text.split('\n')
+        board.remove('GRID START')
+        board.remove('GRID STOP')
+        rows = len(board)
+       
+        # Create the list that will be used in the solver function.
 
-    
-    read_grid = bff.read()
-    # read file and find the grid part
-    # Use re.DOTALL to get the part of the file from 'GRID START from *GRID STOP'
-    grid = re.search('GRID START.*GRID STOP', read_grid, re.DOTALL)
-    grid_text = read_grid[grid.start():grid.end()]
-    bff.close()
+        GRID = [[0 for i in range(2 * col + 1)]
+                for j in range(2 * rows + 1)]
+        # change the number of responding position
 
-    # calculate the size of our grid
-    rows = 0
-    col = 0
+        a = -1
+        for i in board:
+            a = a + 1
+            k = 0
+            for j in i:
+                if j == 'o':
+                    k += 1
+                    GRID[2 * a + 1][2 * k - 1] = 1
+                if j == 'A':
+                    k += 1
+                    GRID[2 * a + 1][2 * k - 1] = 2
+                if j == 'B':
+                    k += 1
+                    GRID[2 * a + 1][2 * k - 1] = 3
+                if j == 'C':
+                    k += 1
+                    GRID[2 * a + 1][2 * k - 1] = 4
+                if j == 'x':
+                    k += 1
+                    GRID[2 * a + 1][2 * k - 1] = 5
 
-    # We will use re.search to find the first row of the grid.
-    # The number of columns will be calculated with the number of elements 
-    # in the first row.
-    row = re.search('([oxABC] *)+[oxABC]', read_grid)
-    row = read_grid[row.start():row.end()]
-    row=row.replace(' ','')
-    col=len(row)
-    
+        points = re.findall('P \\d \\d', read_grid)
+        points_position = []
+       
+        # Use re.findall to get the points position.
+       
+        for i in points:
+            i = i.replace(' ', '')
+            x = int(i[1])
+            y = int(i[2])
+            points_position.append((x, y))
+        
+        # Get the lasers positions and directions from read_grid
+        # and then put the information in a dictionary.
+        
+        lasers = {}
+        laser = re.findall('L \\d \\d .*\\d .*\\d', read_grid)
+        p = []
+        d = []
 
-    # creat a list and make each line of grid a element
-    # to calculate the number of rows
-    
-  
-    board = grid_text.split('\n')
-    board.remove('GRID START')
-    board.remove('GRID STOP')
-    
-    rows = len(board)
-    
-
-    # Create the list that will be used in the solver function.
-    GRID = [
-        [0 for i in range(2 * col + 1)]
-        for j in range(2 * rows + 1)
-    ]
-
-    # change the number of responding position
-    a = -1
-    for i in board:
-        a =a + 1
-        k = 0
-        for j in i:
-            if j == 'o':
-                k += 1
-                GRID[2 * a + 1][2 * k - 1] = 1
-            if j == 'A':
-                k += 1
-                GRID[2 * a + 1][2 * k - 1] = 2
-            if j == 'B':
-                k += 1
-                GRID[2 * a + 1][2 * k - 1] = 3
-            if j == 'C':
-                k += 1
-                GRID[2 * a + 1][2 * k - 1] = 4
-            if j == 'x':
-                k += 1
-                GRID[2 * a + 1][2 * k - 1] = 5
-
-    # Use re.findall to get the points position.
-    
-    points = re.findall('P \\d \\d', read_grid)
-    
-   
-    
-    points_position = []
-    #The for loop is used to put the points in form of tuples in a list 
-    
-    
-    for i in points:
-        i=i.replace(' ','')
-        x = int(i[1])
-        y = int(i[2])
-        points_position.append((x, y))
-      
-
-    # Get the lasers positions and directions from read_grid
-    # and then put the information in a dictionary.
-    lasers = {}
-    laser = re.findall('L \\d \\d .*\\d .*\\d', read_grid)
-    
-    
-    p = []
-    d = []
-    for i in laser:
-        i = i.split(' ')
-        x_pos = int(i[1])
-        y_pos = int(i[2])
-        p.append((x_pos, y_pos))
-        lasers['position'] = p
-        x_dir = int(i[3])
-        y_dir = int(i[4])
-        d.append((x_dir, y_dir))
-        lasers['direction'] = d
-
-    # Get the blocks information and put it in a dictionary.
-    blocks = {}
-    block = re.findall('[ABC] \\d', read_grid)
-    
-    for i in block:
-        i=i.replace(' ','')
-        blocks[i[0]] = int(i[1])
-   
-
-    return (GRID, blocks, lasers, points_position)
-
-
+        for i in laser:
+            i = i.split(' ')
+            x_pos = int(i[1])
+            y_pos = int(i[2])
+            p.append((x_pos, y_pos))
+            lasers['position'] = p
+            x_dir = int(i[3])
+            y_dir = int(i[4])
+            d.append((x_dir, y_dir))
+            lasers['direction'] = d
+        
+        
+        # Get the blocks information and put it in a dictionary.
+        
+        blocks = {}
+        block = re.findall('[ABC] \\d', read_grid)
+        for i in block:
+            i = i.replace(' ', '')
+            blocks[i[0]] = int(i[1])
+        return (GRID, blocks, lasers, points_position)
 
 
 def laser_path(position, direction, board, blocks):
@@ -225,7 +214,7 @@ def run_all_blocks_comb(blocks,GRID,index,cur_blocks,lasers,points_position):
     if ('A' not in blocks or blocks['A']==0) and ('B' not in blocks or blocks['B']==0) and ('C' not in blocks or blocks['C']==0):
         if(run_pro(lasers,points_position,GRID,cur_blocks) == True):
             print(cur_blocks)
-        return run_pro(lasers,points_position,GRID,cur_blocks)
+            return run_pro(lasers,points_position,GRID,cur_blocks)
     for i in range(index,lens):
         col_index = (int)(i/len(GRID[0]))
         row_index = (int)(i%len(GRID[0]))
@@ -261,6 +250,7 @@ def save(GRID, lasers, points_position, cur_blocks):
     Block = []
 
     # This for loop is used to form the Grid like in the lazor app.
+    print(GRID)
     for i in range(0, len(GRID)):
         # The reading function give us a series of numbers.
         # When the list is full of zeros means that t
@@ -288,6 +278,7 @@ def save(GRID, lasers, points_position, cur_blocks):
     solA = []
     solB = []
     solC = []
+    print(Block)
     # The for loop will be used to adjust the blocks in the GRID.
     if not A:
         pass
@@ -419,25 +410,25 @@ def get_colors():
 if __name__ == '__main__':
 
     filename = input('Please enter the filename you want to solve: ')
-    Read = read_file(filename)
-    if Read != None:
-        GRID = Read[0]
-        blocks = Read[1]
-        lasers = Read[2]
-        points_position = Read[3]
-        cur_blocks = {}
-        cur_blocks['A'] = []
-        cur_blocks['B'] = []
-        cur_blocks['C'] = []
-        for i in range(len(GRID)):
-            for j in range(len(GRID[0])):
-                if GRID[i][j] == 2:
+    r=File(filename)
+    Read = r.read_file()
+    GRID = Read[0]
+    blocks = Read[1]
+    lasers = Read[2]
+    points_position = Read[3]
+    cur_blocks = {}
+    cur_blocks['A'] = []
+    cur_blocks['B'] = []
+    cur_blocks['C'] = []
+    for i in range(len(GRID)):
+        for j in range(len(GRID[0])):
+            if GRID[i][j] == 2:
                     cur_blocks['A'].append((i,j))
                     if GRID[i][j] == 3:
                         cur_blocks['B'].append((i,j))
                         if GRID[i][j] == 4:
                             cur_blocks['C'].append((i,j))
-        (run_all_blocks_comb(blocks,GRID,0,cur_blocks,lasers,points_position))
-        c=save(GRID,lasers,points_position,cur_blocks)
+    print(run_all_blocks_comb(blocks,GRID,0,cur_blocks,lasers,points_position))
+    c=save(GRID,lasers,points_position,cur_blocks)
         
    
